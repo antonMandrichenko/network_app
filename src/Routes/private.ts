d4ce44e6ea49caf34357420754e42ec689617e2a
Anton <arnellomutty2@gmail.com>
2020-03-24 11:49:05 +0200

    Deleted not needed comments from private routes

--- src/Routes/private.ts
+++ src/Routes/private.ts
@@ -1,12 +1,4 @@
-import { ParameterizedContext } from 'koa'
 import Router from 'koa-router'
-
-import get from 'lodash/get'
-
-// import signGmailPackage from 'utils/SocialPackages/Gmail/signGmailPackage'
-// import gmailPackageUploaded from 'utils/SocialPackages/Gmail/gmailPackageUploaded'
-
-// import { uploadProfilePhoto, uploadCommunityPhoto } from 'utils/Images'
 
 const privateRouter = new Router()
 
